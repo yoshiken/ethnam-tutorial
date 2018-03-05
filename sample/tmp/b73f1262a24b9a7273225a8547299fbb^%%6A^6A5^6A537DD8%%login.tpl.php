b8d534b00541f813040d3e23385a1aa73a311059
yskn <yskn.beer@gmail.com>
2018-03-05 15:51:15 +0900

    ログイン画面作成

---
+++ sample/tmp/b73f1262a24b9a7273225a8547299fbb^%%6A^6A5^6A537DD8%%login.tpl.php
@@ -0,0 +1,17 @@
+<?php /* Smarty version 2.6.31, created on 2018-03-05 15:50:32
+         compiled from login.tpl */ ?>
+<form action="." method="post">
+  <table border="0">
+    <tr>
+      <td>メールアドレス</td>
+      <td><input type="text" name="mailaddress" value=""></td>
+    </tr>
+    <tr>
+      <td>パスワード</td>
+      <td><input type="password" name="password" value=""></td>
+    </tr>
+  </table>
+  <p>
+  <input type="submit" name="action_login_do" value="ログイン">
+  </p>
+</form>
